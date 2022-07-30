8a54af30c87caa3f3cab71dd416cb28378d8fbc2
Julian <juliantaco23@gmail.com>
2022-07-30 07:59:59 -0500

    delete and edit added

--- models/utils.ts
+++ models/utils.ts
@@ -17,6 +17,19 @@ export default class Utils {
 		this.saveTaskInLS();
 	}
 
+	deleteTask(taskToDeleteId: number): void{
+		let indexOfElement: number = this.tasks.findIndex(element => element.taskId == taskToDeleteId);
+		console.log(taskToDeleteId,"ssxsxs",indexOfElement);
+		this.tasks.splice(indexOfElement, 1);
+		this.saveTaskInLS();
+	};
+
+	editTask(taskToEditId: number, newText: string): void{
+		let taskWithId: any = this.tasks.find(element => element.taskId == taskToEditId);
+		taskWithId.taskName = newText;
+		this.saveTaskInLS;
+	};
+
 	getTasks(): Array<Task> {
 		return this.tasks;
 	}
